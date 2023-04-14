31f4b427d0fd7b8663bed17fecb45b357b7edbee
Ashton <128860851+katawarren008@users.noreply.github.com>
2023-04-14 16:31:57 -0700

    # 7 DONE

--- group_6_7.py
+++ group_6_7.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env/ python3
 """The program prints a basic multiplication table.
     """
+N = 1
+while N <= 12:
+    M = 1
+    while M <= 12:
+        print(N, "*", M, "=", N * M)
+        M += 1
+    N += 1
